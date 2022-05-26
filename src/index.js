e688e6723ade37d029ff9c4f57083c483d7432b2
Kevin <NateraKevin1@gmail.com>
2022-05-26 18:21:10 -0400

    made menu buttons appear

--- src/index.js
+++ src/index.js
@@ -15,20 +15,27 @@ document.addEventListener("DOMContentLoaded", () => {
     const menuCTX = menuCanvas.getContext("2d");
     
     menuCanvas.style.position = "absolute";
-    menuCanvas.style.top = "400px";
-    menuCanvas.style.left= "100px";
+    menuCanvas.style.top = "450px";
+    menuCanvas.style.left= "110px";
 
-    menuCTX.fillStyle = "blue"
-    menuCTX.fillRect(0,0,menuCanvas.width, menuCanvas.height )
+    // menuCTX.fillStyle = "blue"
+    // menuCTX.fillRect(0,0,menuCanvas.width, menuCanvas.height )
 
-    const playerSpriteSheet = new Image()
-    playerSpriteSheet.src = "../assets/spritesheets/warrior_spritesheet.png"
-    playerSpriteSheet.onload - function() {
-        animate();
+    const basicIconSpritesheet = new Image()
+    basicIconSpritesheet.src = "../assets/spritesheets/basic_icon_spritesheet.png"
+    basicIconSpritesheet.onload = function() {
+        drawButtons();
     }
 
+    function drawButtons() {
+        let buttonWidth = 514
+        let buttonHeight = 514
+        let offset = 519
+
+        menuCTX.drawImage(basicIconSpritesheet, offset, 0, buttonWidth, buttonHeight, 25, 25, 100, 100) //attack button
+        menuCTX.drawImage(basicIconSpritesheet, 0, 0, buttonWidth, buttonHeight, 175, 25, 100, 100) //ability button
+        menuCTX.drawImage(basicIconSpritesheet, offset, offset, buttonWidth, buttonHeight, 325, 25, 100, 100) //items button
+        menuCTX.drawImage(basicIconSpritesheet, 0, offset, buttonWidth, buttonHeight, 475, 25, 100, 100) //defend button
+    }
 });
 
-function animate() {
-    
-}
